d6823533a6b90d03c052c919634c4484dec01b56
MigVarona <migvaronag@gmail.com>
2025-02-11 18:01:21 +0100

    pr

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,19 +2,22 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="border-b border-gray-100">
-      <div className="container mx-auto px-4 py-6">
-        <nav className="flex items-center justify-center">
-        <Link
-  href="/"
-  className="h-full text-4xl sm:text-5xl md:text-6xl text-[rgb(34,34,34)] font-bold flex items-center font-noto tracking-[0.25em]"
->
-  RENEW
-</Link>
-
-        </nav>
-      </div>
-    </header>
+    <>
+      <header className="border-b border-gray-100">
+        <div className="container mx-auto px-4 py-6">
+          <nav className="flex items-center justify-center">
+            <Link
+              href="/"
+              className="h-full text-4xl sm:text-5xl md:text-6xl text-[rgb(34,34,34)] font-bold flex items-center font-noto tracking-[0.25em]"
+            >
+              RENEW
+            </Link>
+          </nav>
+        </div>
+      </header>
+      {/* Línea gris de ancho completo */}
+      <hr className="w-full border-[1px] border-gray-300" />
+      </>
   );
 };
 

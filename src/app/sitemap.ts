555808184494aa8db2376008b39d9258b61b03cc
migvarona <migvaronag@gmail.com>
2025-03-01 21:24:02 +0100

    pr

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,6 @@ import { MetadataRoute } from "next"
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://renewhabits.com"
 
-  // Suponiendo que tienes una API que devuelve tus artículos
   const articles = await fetch(`${siteUrl}/api/articles`).then((res) => res.json())
 
   const articleUrls = articles.map((article: { slug: string }) => ({
